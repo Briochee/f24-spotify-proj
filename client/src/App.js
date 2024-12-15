3d684f8c2852b9d830d5313dd7135fdb0815689a
Rio Simpson <rio.simpsonnyc@gmail.com>
2024-12-15 23:26:09 +0100

    misc

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
+// website constants
+import NavDropdown from "./components/navigate/navigate.js";
+
+// webpages
+import Login from "./components/login/login.js";
+import Home from "./components/homepage/homepage.js";
+import Callback from "./components/callback/callback.js";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+              <div className="App">
+                  <Routes>
+                      <Route path="/login" element={<Login />} />
+                      <Route path="/homepage" element={<Home />} />
+                      <Route path="/spotify-callback" element={<Callback />} />
+                  </Routes>
+              </div>
+          </Router>
+        </div>
+    );
 }
 
 export default App;
