b30ad923a7acba426405e0278ebea4e3ba14a76b
Rio Simpson <rio.simpsonnyc@gmail.com>
2024-12-17 18:14:20 +0100

    updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 // webpages
 import Login from "./components/login/login.js";
 import Home from "./components/homepage/homepage.js";
-import Quiz from "./components/quiz/quiz.js";
+import QuizOptions from "./components/quiz/quizOptions.js";
 import Profile from "./components/profile/profile.js";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
                   <Routes>
                       <Route path="/login" element={<Login />} />
                       <Route path="/homepage" element={<Home />} />
-                      <Route path="/quiz" element={<Quiz />} />
+                      <Route path="/quiz-options" element={<QuizOptions />} />
                       <Route path="/profile" element={<Profile />} />
                   </Routes>
               </div>
