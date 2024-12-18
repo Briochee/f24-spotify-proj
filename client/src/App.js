75f24bc01beda9d40f5f2c3f0b88266be5110a38
Yana Kuzmia <111184261+yanii2@users.noreply.github.com>
2024-12-18 13:34:31 +0100

    added import

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './App.css';
 
 // website constants
@@ -17,7 +18,7 @@ function App() {
             <Router>
               <div className="App">
                   <Routes>
-                        <Route path="/" element={<Navigate to="/login" />} />
+                        <Route path="/" element={<useNavigate to="/login" />} />
 
                         <Route path="/login" element={<Login />} />
                         <Route path="/homepage" element={<Home />} />
