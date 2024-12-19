9e250e99d8ca26b53c7badcd058fb123f6e429ef
Yana Kuzmia <111184261+yanii2@users.noreply.github.com>
2024-12-19 20:19:56 +0100

    modifications to work with verifyanswer logic and save session values

--- client/src/components/quiz/quiz.js
+++ client/src/components/quiz/quiz.js
@@ -1,29 +1,26 @@
-import React, { useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef, useCallback} from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import {
     getRandomPlaylistTracks,
-    getPlaylistDetails,
     loadSpotifyPlayer,
     playSongSnippet,
 } from "../spotify/spotifyFunctions";
+import { verifyAnswer } from "../quiz/quizQuestions";
 import NavDropdown from "../navigate/navigate.js";
 import GameBar from "../gamebar/gamebar.js";
 import "./quiz.css"
 
 const Quiz = () => {
-    const [playlistDetails, setPlaylistDetails] = useState({ name: "", image: null });
     const [songs, setSongs] = useState([]);
     const [deviceId, setDeviceId] = useState(null);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [userInput, setUserInput] = useState({ song: "", artist: "" });
-    const [points, setPoints] = useState(0);
     const [quizOver, setQuizOver] = useState(false);
     const [loading, setLoading] = useState(true);
     const [inputsDisabled, setInputsDisabled] = useState(false);
     const [scoreMessage, setScoreMessage] = useState("");
     const [difficulty, setDifficulty] = useState("easy");
-    const [albumCoverRevealed, setAlbumCoverRevealed] = useState(false);
     const [isRevealed, setIsRevealed] = useState(false);
     const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
     const [incorrectAnswersCount, setIncorrectAnswersCount] = useState(0);
@@ -31,12 +28,64 @@ const Quiz = () => {
     const [gameBarRefreshKey, setGameBarRefreshKey] = useState(0);
     // eslint-disable-next-line no-unused-vars
     const [snippetPlayCount, setSnippetPlayCount] = useState(0);
+    // eslint-disable-next-line no-unused-vars
+    const [isConnectedToPlayback, setIsConnectedToPlayback] = useState(false);
+
 
     const location = useLocation();
     const navigate = useNavigate();
 
     const songInputRef = useRef(null);
     const artistInputRef = useRef(null);
+
+    const pointsRef = useRef(0);
+
+    const saveQuizHistory = (finalPoints) => {
+        const token = localStorage.getItem("token");
+        
+        const data = {
+            quizzesTaken: 1,
+            questionsAnswered: correctAnswersCount + incorrectAnswersCount,
+            lifetimeScore: finalPoints,
+            correctAnswers: correctAnswersCount,
+            incorrectAnswers: incorrectAnswersCount,
+        };
+        
+        try {
+            axios.put(`${process.env.REACT_APP_BACKEND_URL}/api/users/quiz-history`, data, {
+                headers: { 
+                    Authorization: `Bearer ${token}` 
+                },
+            });
+            // console.log("Quiz History Saved, Points: ", finalPoints);
+            setGameBarRefreshKey((prevKey) => prevKey + 1);
+        } catch (error) {
+            console.error("Error updating quiz history:", error);
+        }
+    };
+
+    const initializeQuiz = useCallback(async (playlistId, size) => {
+        try {
+            setLoading(true);
+    
+            const tracks = await getRandomPlaylistTracks(playlistId, size);
+            setSongs(tracks);
+    
+            const spotifyTokens = JSON.parse(localStorage.getItem("spotifyTokens"));
+    
+            if (!isConnectedToPlayback) {
+                const deviceId = await loadSpotifyPlayer(spotifyTokens.accessToken);
+                setDeviceId(deviceId);
+            }
+    
+            setIsConnectedToPlayback(true);
+        } catch (error) {
+            console.error("Error initializing quiz:", error.message);
+            setIsConnectedToPlayback(false);
+        } finally {
+            setLoading(false);
+        }
+    }, [isConnectedToPlayback]);
 
     useEffect(() => {
         const params = new URLSearchParams(location.search);
@@ -49,7 +98,7 @@ const Quiz = () => {
         if (playlistId && quizSizeParam) {
             initializeQuiz(playlistId, parseInt(quizSizeParam, 10));
         }
-    }, [location]);
+    }, [location, initializeQuiz]);
 
     useEffect(() => {
         setInputsDisabled(false);
@@ -63,6 +112,119 @@ const Quiz = () => {
         }, 0);
     }, [currentQuestionIndex]);
 
+    const handlePlaySnippet = (trackUri) => {
+        if (deviceId) {
+        
+            let snippetDuration = 5000; // Default: 5s
+            if (difficulty === "easy") {
+                snippetDuration = 10000;    // Easy: 10s
+            } else if (difficulty === "medium"){
+                snippetDuration = 5000; // Medium: 5s
+            } else if (difficulty === "hard") {
+                snippetDuration = 2000; // Hard: 2s
+            }
+        
+            setIsSnippetPlaying(true);
+            
+            try {
+                playSongSnippet(deviceId, trackUri, snippetDuration);
+                setTimeout(() => {
+                    setIsSnippetPlaying(false);
+                }, snippetDuration);
+            } catch (error) {
+                setIsSnippetPlaying(false);
+                console.error("Error playing snippet:", error);
+            }
+
+            setSnippetPlayCount((prevCount) => {
+                const newCount = prevCount + 1;
+                if ((difficulty === "easy" && newCount > 3) || (difficulty === "medium" && newCount > 2) || (difficulty === "hard" && newCount > 1)) {
+                    pointsRef.current--;
+                }
+                return newCount;
+            });
+        } else {
+            console.error("Spotify Player not initialized");
+            return;
+        }
+    };
+
+    const moveToNextQuestion = () => {
+        setScoreMessage("");
+        setIsRevealed(false);
+        if (currentQuestionIndex + 1 < songs.length) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1);
+            setSnippetPlayCount(0);
+            setUserInput({ song: "", artist: "" });
+            setInputsDisabled(false);
+        } else {
+            const finalPoints = pointsRef.current;
+            saveQuizHistory(finalPoints);
+            setQuizOver(true);
+        }
+    };
+
+    const handleSubmit = () => {
+        const token = localStorage.getItem("token");
+        const userId = JSON.parse(atob(token.split(".")[1])).id;
+        const currentSong = songs[currentQuestionIndex]?.track;
+    
+        if (!currentSong || !currentSong.artists || currentSong.artists.length === 0) {
+            setScoreMessage("Invalid song data. Skipping question.");
+            moveToNextQuestion();
+            return;
+        }
+    
+        const { scoreIncrement, penalty, feedbackMessage, isCorrect } = verifyAnswer({
+            currentSong, userInput, difficulty, isRevealed,
+        });
+        
+        let increment = scoreIncrement - penalty;
+        pointsRef.current += increment;
+
+        const currentSessionScore = parseInt(localStorage.getItem(`sessionScore_${userId}` || 0, 10));
+        const updatedSessionScore = currentSessionScore + increment;
+        localStorage.setItem(`sessionScore_${userId}`, updatedSessionScore);
+        setGameBarRefreshKey((prevKey) => prevKey + 1);
+        // console.log("New Points after verify: ", pointsRef.current);
+    
+        if (isCorrect) {
+            setCorrectAnswersCount((prev) => prev + 1);
+        } else {
+            setIncorrectAnswersCount((prev) => prev + 1);
+        }
+    
+        setScoreMessage(feedbackMessage);
+        setTimeout(moveToNextQuestion, 1500);
+    };
+
+    const handleRetry = async () => {
+        setQuizOver(false);
+        pointsRef.current = 0;
+        setCorrectAnswersCount(0);
+        setIncorrectAnswersCount(0);
+        setCurrentQuestionIndex(0);
+    
+        try {
+            setLoading(true);
+            const params = new URLSearchParams(location.search);
+            const playlistId = params.get("playlistId");
+            if (!playlistId) {
+                console.error("Playlist ID is missing. Cannot retry quiz.");
+                setLoading(false);
+                return;
+            }
+
+            const newSongs = await getRandomPlaylistTracks(playlistId, songs.length);
+            setSongs(newSongs);
+        } catch (error) {
+            console.error("Error retrying quiz:", error.message);
+        } finally {
+            setLoading(false);
+            setGameBarRefreshKey((prevKey) => prevKey + 1);
+        }
+    };
+
     const handleKeyDown = (e, nextInputRef) => {
         if (e.key === "Enter") {
             if (nextInputRef && nextInputRef.current) {
@@ -73,195 +235,14 @@ const Quiz = () => {
         }
     };
 
-    const initializeQuiz = async (playlistId, size) => {
-        try {
-            setLoading(true);
-
-            const details = await getPlaylistDetails(playlistId);
-            setPlaylistDetails(details);
-
-            const tracks = await getRandomPlaylistTracks(playlistId, size);
-            setSongs(tracks);
-
-            const spotifyTokens = JSON.parse(localStorage.getItem("spotifyTokens"));
-            const deviceId = await loadSpotifyPlayer(spotifyTokens.accessToken);
-            setDeviceId(deviceId);
-        } catch (error) {
-            console.error("Error initializing quiz:", error.message);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const handlePlaySnippet = async (trackUri) => {
-        if (deviceId) {
-            let snippetDuration = 10000; // Easy: 10s
-            if (difficulty === "medium") snippetDuration = 5000; // Medium: 5s
-            else if (difficulty === "hard") snippetDuration = 2000; // Hard: 2s
-    
-            setIsSnippetPlaying(true);
-    
-            // Play the snippet
-            await playSongSnippet(deviceId, trackUri, snippetDuration);
-    
-            setTimeout(() => {
-                setIsSnippetPlaying(false);
-            }, snippetDuration);
-    
-            setSnippetPlayCount((prevCount) => {
-                const newCount = prevCount + 1;
-    
-                if (
-                    (difficulty === "easy" && newCount > 3) ||
-                    (difficulty === "medium" && newCount > 2) ||
-                    (difficulty === "hard" && newCount > 1)
-                ) {
-                    setPoints((prevPoints) => Math.max(prevPoints - 1));
-                }
-    
-                return newCount;
-            });
-        } else {
-            console.error("Spotify Player not initialized");
-        }
-    };
-
-    const handleSubmit = () => {
-        const currentSong = songs[currentQuestionIndex]?.track;
-    
-        const correctSong = currentSong.name.toLowerCase();
-        const correctArtist = currentSong.artists[0].name.toLowerCase();
-    
-        const inputSong = userInput.song.toLowerCase().trim();
-        const inputArtist = userInput.artist.toLowerCase().trim();
-    
-        let scoreIncrement = 0;
-    
-        // If no input is provided
-        if (!inputSong && !inputArtist) {
-            const penalty = difficulty === "easy" ? 2 : difficulty === "medium" ? 3 : 5;
-            setScoreMessage(`No input provided! ${penalty} points deducted.`);
-            setPoints((prevPoints) => Math.max(prevPoints - penalty, 0));
-            setIncorrectAnswersCount((prev) => prev + 1);
-            moveToNextQuestion();
-            return;
-        }
-    
-        // If one or both inputs are incorrect
-        if (inputSong !== correctSong || inputArtist !== correctArtist) {
-            const penalty = !inputSong || !inputArtist
-                ? difficulty === "easy" ? 1 : difficulty === "medium" ? 2 : 3 
-                : difficulty === "easy" ? 1 : difficulty === "medium" ? 3 : 5;
-    
-            setScoreMessage(`Incorrect! ${penalty} points deducted.`);
-            setPoints((prevPoints) => Math.max(prevPoints - penalty, 0));
-            setIncorrectAnswersCount((prev) => prev + 1);
-        }
-    
-        // Full correct answer
-        if (inputSong === correctSong && inputArtist === correctArtist) {
-            if (isRevealed) {
-                scoreIncrement = difficulty === "easy" ? 2 : difficulty === "medium" ? 3 : 10;
-            } else {
-                scoreIncrement = 10;
-            }
-            setCorrectAnswersCount((prev) => prev + 1);
-            setScoreMessage(`Score increased by ${scoreIncrement}!`);
-        }
-        // Partial correct answer
-        else if (inputSong === correctSong || inputArtist === correctArtist) {
-            if (isRevealed) {
-                scoreIncrement = difficulty === "easy" ? 2 : difficulty === "medium" ? 3 : 5;
-            } else {
-                scoreIncrement = 5;
-            }
-            setCorrectAnswersCount((prev) => prev + 1);
-            setScoreMessage(`Score increased by ${scoreIncrement}!`);
-        }
-    
-        // Update points after scoring logic
-        setPoints((prevPoints) => prevPoints + scoreIncrement);
-    
-        moveToNextQuestion();
-    };
-
-    const moveToNextQuestion = () => {
-        setTimeout(() => {
-            setScoreMessage("");
-            setAlbumCoverRevealed(false);
-            setIsRevealed(false);
-            if (currentQuestionIndex + 1 < songs.length) {
-                setCurrentQuestionIndex(currentQuestionIndex + 1);
-                setSnippetPlayCount(0);
-                setUserInput({ song: "", artist: "" });
-                setInputsDisabled(false);
-            } else {
-                handleQuizCompletion();
-            }
-        }, 1500);
-    };
-
-    const handleQuizCompletion = async () => {
-        setQuizOver(true);
-        localStorage.setItem("sessionScore", points);
-    
-        try {
-            const token = localStorage.getItem("token");
-            const data = {
-                quizzesTaken: 1,
-                questionsAnswered: correctAnswersCount + incorrectAnswersCount,
-                lifetimeScore: points,
-                correctAnswers: correctAnswersCount,
-                incorrectAnswers: incorrectAnswersCount,
-            };
-    
-            await axios.put(`${process.env.REACT_APP_BACKEND_URL}/api/users/quiz-history`, data, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-    
-            console.log("Quiz history updated successfully!");
-        } catch (error) {
-            console.error("Error updating quiz history:", error.message);
-        }
-    };
-
-
     const handleAlbumReveal = () => {
         if (difficulty !== "hard") {
-            setAlbumCoverRevealed(true);
             setIsRevealed(true);
         }
     };
 
-    const handleRetry = async () => {
-        setQuizOver(false);
-        setPoints(0);
-        setCorrectAnswersCount(0);
-        setIncorrectAnswersCount(0);
-        setCurrentQuestionIndex(0);
-    
-        try {
-            setLoading(true);
-            if (!playlistDetails?.id) {
-                console.error("Playlist ID is missing. Cannot retry quiz.");
-                setLoading(false);
-                return;
-            }
-    
-            const newSongs = await getRandomPlaylistTracks(
-                playlistDetails.id,
-                songs.length
-            );
-    
-            setSongs(newSongs);
-        } catch (error) {
-            console.error("Error retrying quiz:", error.message);
-        } finally {
-            setLoading(false);
-            setGameBarRefreshKey((prevKey) => prevKey + 1); // Increment refresh key
-        }
+    const handleNewQuiz = () => { 
+        navigate("/quiz-options");
     };
 
     const getBlurClass = () => {
@@ -273,14 +254,14 @@ const Quiz = () => {
     return (
         <div>
             <NavDropdown />
-            <GameBar sessionScore={points} refreshKey={gameBarRefreshKey} />            <div className="container">
+            <GameBar refreshKey={gameBarRefreshKey} />            <div className="container">
                 {loading ? (
                     <p>Loading quiz...</p>
                 ) : quizOver ? (
                     <div className="quiz-over-container">
                         <h1>Quiz Over!</h1>
-                        <p>Your Score: {points}</p>
-                        <button onClick={() => navigate("/quiz-options")} className="quiz-button">
+                        <p>Your Score: {pointsRef.current}</p>
+                        <button onClick={handleNewQuiz} className="quiz-button">
                             New Quiz
                         </button>
                         <button onClick={handleRetry} className="quiz-button">
@@ -296,7 +277,7 @@ const Quiz = () => {
                             {songs[currentQuestionIndex]?.track.album.images[0]?.url && (
                                 <div
                                     className={`album-cover ${
-                                        albumCoverRevealed ? "revealed" : getBlurClass()
+                                        isRevealed ? "revealed" : getBlurClass()
                                     }`}
                                     style={{
                                         backgroundImage: `url(${songs[currentQuestionIndex]?.track.album.images[0]?.url})`,
@@ -317,10 +298,10 @@ const Quiz = () => {
                             {difficulty !== "hard" && (
                                 <button
                                     onClick={handleAlbumReveal}
-                                    className={`reveal-button ${albumCoverRevealed ? "disabled" : ""}`}
-                                    disabled={albumCoverRevealed}
+                                    className={`reveal-button ${isRevealed ? "disabled" : ""}`}
+                                    disabled={isRevealed}
                                 >
-                                    {albumCoverRevealed ? "Revealed" : "Reveal"}
+                                    {isRevealed ? "Revealed" : "Reveal"}
                                 </button>
                             )}
                         </div>
